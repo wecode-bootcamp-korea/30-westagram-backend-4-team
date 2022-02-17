a5d3ce628ce5f5620d738ea629560cd50893de3a
ahn <sungjoonan02@gmail.com>
2022-02-17 18:00:34 +0900

    Changed: class renamed to User and use snake case

--- students/sungjoon/users/models.py
+++ students/sungjoon/users/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-class Username(models.Model):
-    first_name = models.CharField(max_length=45)
-    last_name  = models.CharField(max_length=45)
-    email      = models.EmailField(max_length=254, unique=True)
-    password   = models.CharField(max_length=150)
-    cellphone  = models.CharField(max_length=30)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class User(models.Model):
+    username     = models.CharField(max_length=50)
+    first_name   = models.CharField(max_length=45)
+    last_name    = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=254, unique=True)
+    password     = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=30)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'usernames'
+        db_table = 'users'
