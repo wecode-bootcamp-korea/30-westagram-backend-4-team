c318e0f68a4b78960d25e6d2a764fd05ac91b0b6
tanonkim <robbins4bos@gmail.com>
2022-02-17 18:25:25 +0900

    Fix : table name, Class name, and Delete default property

--- students/kihyeon/users/models.py
+++ students/kihyeon/users/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class UserInformation(models.Model):
+class User(models.Model):
     first_name   = models.CharField(max_length=50)
     last_name    = models.CharField(max_length=50, null=True)
     email        = models.EmailField(max_length=50, unique=True)
-    password     = models.CharField(max_length=100, default=0, unique=True)
+    password     = models.CharField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=15)
     created_at   = models.DateTimeField(auto_now_add=True, null=True)
     updated_at   = models.DateTimeField(auto_now=True, null=True)
     
     class Meta:
-        db_table = "userinfo"
+        db_table = "userinfos"
