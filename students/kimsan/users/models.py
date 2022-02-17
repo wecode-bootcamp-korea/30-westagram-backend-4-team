091d4e7e0de9fcf0cbe1973e61300e3ae79c3406
crud1 <cloudhat17@gmail.com>
2022-02-17 17:31:55 +0900

    project implement model

--- students/kimsan/users/models.py
+++ students/kimsan/users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    phone_number=models.IntegerField()
-    extra_info=models.TextField()
+    name = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    password = models.CharField(max_length=50, blank=True, null=True)
+    phone_number=models.IntegerField( blank=True, null=True)
+    extra_info=models.TextField( blank=True, null=True)
     class Meta:
         db_table = 'users'
 
