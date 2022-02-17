0e8abf7c517982509ea9e4a6512eadbbc6ae11f0
ahn <sungjoonan02@gmail.com>
2022-02-17 17:30:18 +0900

    Changed: ID Username으로 변경 및 모델링 수정

--- students/sungjoon/users/models.py
+++ students/sungjoon/users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-class User(models.Model):
-    name      = models.CharField(max_length=45)
-    email     = models.EmailField(max_length=254)
-    password  = models.CharField(max_length=30)
-    cellphone = models.CharField(max_length=15)
-
+class Username(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name  = models.CharField(max_length=45)
+    email      = models.EmailField(max_length=254, unique=True)
+    password   = models.CharField(max_length=150)
+    cellphone  = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'users'
+        db_table = 'usernames'
