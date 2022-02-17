dbf6737001eb16679928dd42b58571b3f18e0dcf
tanonkim <robbins4bos@gmail.com>
2022-02-17 17:13:52 +0900

    Fix : username, property in Field, adding uniqueness in email field

--- students/kihyeon/users/models.py
+++ students/kihyeon/users/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class UserInformation(models.Model):
-    name         = models.CharField(max_length=50)
-    email        = models.EmailField(max_length=50)
-    password     = models.CharField(max_length=50, default=0)
-    phone_number = models.IntegerField()
+    first_name   = models.CharField(max_length=50)
+    last_name    = models.CharField(max_length=50, null=True)
+    email        = models.EmailField(max_length=50, unique=True)
+    password     = models.CharField(max_length=100, default=0, unique=True)
+    phone_number = models.CharField(max_length=15)
+    created_at   = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at   = models.DateTimeField(auto_now=True, null=True)
     
     class Meta:
         db_table = "userinfo"
