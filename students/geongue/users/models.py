ca535c95b86bd68fb2c6a33ece0ea6d78399ca47
Barney <holyja@naver.com>
2022-02-17 18:26:12 +0900

    Fix: Field data modify in models.py

--- students/geongue/users/models.py
+++ students/geongue/users/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-	name = models.CharField(max_length=45)
-	email = models.EmailField(max_length=254)
-	password = models.CharField(max_length=40)
-	phone = models.CharField(max_length=20)
+	first_name = models.CharField(max_length=45)
+	last_name  = models.CharField(max_length=45)
+	email 	   = models.EmailField(max_length=254, unique = True)
+	password   = models.CharField(max_length=120)
+	phone	   = models.CharField(max_length=20)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 	class Meta:
 		db_table = 'users'
