43a6265063359dc69df247f38984862bc0cfc581
tanonkim <robbins4bos@gmail.com>
2022-02-17 15:59:55 +0900

    Remove : 'max_length' from interger field

--- students/kihyeon/users/models.py
+++ students/kihyeon/users/models.py
@@ -5,7 +5,7 @@ class UserInformation(models.Model):
     name         = models.CharField(max_length=50)
     email        = models.EmailField(max_length=50)
     password     = models.CharField(max_length=50, default=0)
-    phone_number = models.IntegerField(max_length=15)
+    phone_number = models.IntegerField()
     
     class Meta:
         db_table = "userinfo"
