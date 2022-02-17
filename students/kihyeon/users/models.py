05c1894378b1a63c6a014cb0c0f09654b0d87ec4
tanonkim <robbins4bos@gmail.com>
2022-02-17 18:56:39 +0900

    Fix : tablenames and password

--- students/kihyeon/users/models.py
+++ students/kihyeon/users/models.py
@@ -5,10 +5,10 @@ class User(models.Model):
     first_name   = models.CharField(max_length=50)
     last_name    = models.CharField(max_length=50, null=True)
     email        = models.EmailField(max_length=50, unique=True)
-    password     = models.CharField(max_length=100, unique=True)
+    password     = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15)
     created_at   = models.DateTimeField(auto_now_add=True, null=True)
     updated_at   = models.DateTimeField(auto_now=True, null=True)
     
     class Meta:
-        db_table = "members"
+        db_table = "users"
