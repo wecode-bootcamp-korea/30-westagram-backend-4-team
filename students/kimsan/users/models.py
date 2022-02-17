36edf8ce85b8a14f27b637dc5e9cddef94e0db93
crud1 <cloudhat17@gmail.com>
2022-02-17 17:57:32 +0900

    project implement model 수정사항 반영1

--- students/kimsan/users/models.py
+++ students/kimsan/users/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class User(models.Model):
-    first_name  = models.CharField(max_length=20, blank=True, null=True)
-    second_name = models.CharField(max_length=20, blank=True, null=True)
-    email       = models.CharField(max_length=100, blank=True, null=True)
-    password    = models.CharField(max_length=50, blank=True, null=True)
-    phone_number=models.IntegerField( blank=True, null=True)
-    extra_info  =models.TextField( blank=True, null=True)
+    first_name        = models.CharField(max_length=20 )
+    second_name       = models.CharField(max_length=20 )
+    email             = models.CharField(max_length=100)
+    password          = models.CharField(max_length=50)
+    phone_number      = models.IntegerField( )
+    extra_information = models.TextField( blank=True, null=True)
+    
     class Meta:
         db_table = 'users'
 
