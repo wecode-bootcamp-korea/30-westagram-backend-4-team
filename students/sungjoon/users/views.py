1f124ecd16618e3f195f59323f745e216f6bbd87
ahn <sungjoonan02@gmail.com>
2022-02-18 16:10:02 +0900

    Add: views.py in users app and modified urls

--- students/sungjoon/users/views.py
+++ students/sungjoon/users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class UserView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        email_regex    = re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9.]+$', data['email'])
+        password_regex = re.match('^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]){8,}$', data['password'])
+
+        try:
+            if not email_regex or password_regex:
+                return JsonResponse ({"message" : "FORBIDDEN"}, status=403)
+            
+            if User.objects.filter(email = data['email']).exists():
+                return JsonResponse ({"message" : "CONFLICT"}, status=409)
+            
+            User.objects.create(
+                username     = data['username'],
+                password     = data['password'],
+                first_name   = data['first_name'],
+                last_name    = data['last_name'],
+                email        = data['email'],
+                phone_number = data['phone_number']
+            )
+            
+            return JsonResponse({"message" : "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
