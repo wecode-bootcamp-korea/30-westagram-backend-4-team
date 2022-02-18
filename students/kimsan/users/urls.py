4e1553b4a8741d64844a1092a13e9853a39a0f62
crud1 <cloudhat17@gmail.com>
2022-02-18 19:58:11 +0900

    멘토님 코멘트 반영ver2

--- students/kimsan/users/urls.py
+++ students/kimsan/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from users.views import UserView
 
-#cd westagram/30-westagram-backend-4-team/students/kimsan
+
 
 urlpatterns = [
     path('/sign-up', UserView.as_view()),
