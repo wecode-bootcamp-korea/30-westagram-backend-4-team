42fc217d255d149459379ba7327a1321553f6125
Barney <holyja@naver.com>
2022-02-18 15:06:06 +0900

    Add : views.py fix : project urls.py root and app urls.py root change
    SignUp View 작성 완료 및 테스트 진행 완료
    프로젝트 루트사항 'users'로 변경. 앱 루트사항 'signup'으로 변경

--- students/geongue/users/models.py
+++ students/geongue/users/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 class User(models.Model):
-	username   = models.CharField(max_length=45, null=True)
 	first_name = models.CharField(max_length=45)
-	last_name  = models.CharField(max_length=45, null=True)
+	last_name  = models.CharField(max_length=45)
 	email 	   = models.EmailField(max_length=254, unique = True)
 	password   = models.CharField(max_length=120)
 	phone	   = models.CharField(max_length=20)
--- students/geongue/users/views.py
+++ students/geongue/users/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from .models import *
+import json
+import re
 
 # Create your views here.
+
+class SignUpView(View):
+
+    def post(self, request):
+        data       = json.loads(request.body)
+        first_name = data['first_name']
+        last_name  = data['last_name']
+        email      = data['email']
+        password   = data['password']
+        phone      = data['phone']
+        try:
+            email_check = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+
+            if (email_check.match(data["email"]) != None) == False:
+                return JsonResponse({"message" : "Check the email-form"}, status = 400)
+            elif len(password) < 8 or re.search("[a-za]+" ,password) is None or re.search("[0-9]+",
+                 password) is None or re.search("[`~!@#$%^&*(),<.>/?]+", password) is None :
+                return JsonResponse({"message" : "Check the password-form"}, status = 400)
+            elif User.objects.filter(email = email).exists():
+                return JsonResponse({"message" : "Email arleady exist"}, status = 400)
+
+            User.objects.create(first_name = first_name, last_name = last_name, email = email,
+                                 password = password, phone = phone)
+            return JsonResponse({"message" : "SUCCESS"}, status = 201)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+        # data        = json.loads(request.body)
+        # email_check = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+
+        # if data["email"] == None or data["password"] == None :
+        #     return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+        # elif (email_check.match(data["email"]) != None) == False:
+        #     return JsonResponse({"message" : "Check the email-form"}, status = 400)
+        # elif len(data["password"]) < 8 or re.search("[a-za]+" ,data["password"]) is None or re.search("[0-9]+",
+        #          data["password"]) is None or re.search("[`~!@#$%^&*(),<.>/?]+", data["password"]) is None :
+        #     return JsonResponse({"message" : "Check the password-form"}, status = 400)
+        # elif data["email"] == User.object.get(email = data["email"]) :
+        #     return JsonResponse({"message" : "Check the email-form"}, status = 400)
+        # else:
+        #     User.objects.create(first_name = data["first_name"], last_name = data["last_name"], email=data["email"],
+        #                     password=data["password"], phone=data["phone"], create_at=datetime.time())
+        #     return JsonResponse({"mesaage" : "SUCCESS"}, status = 200)
+
