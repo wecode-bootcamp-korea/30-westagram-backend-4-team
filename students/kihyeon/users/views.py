828cffd97cbf3c33003ab1a54e82534d66375865
tanonkim <robbins4bos@gmail.com>
2022-02-21 14:01:02 +0900

    Add : authentificate by using Bcrypt

--- students/kihyeon/users/views.py
+++ students/kihyeon/users/views.py
@@ -1,5 +1,4 @@
-import re
-import json
+import json, bcrypt
 
 from django.http  import JsonResponse
 from django.views import View
@@ -15,6 +14,7 @@ class SignUpView(View):
             email        = data['email']
             password     = data['password']
             phone_number = data['phone_number']
+            secret_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
             if not validate_email(email):
                 return JsonResponse({'Message' : 'Invalid Email'}, status = 400)
@@ -29,7 +29,7 @@ class SignUpView(View):
                 first_name   = data["first_name"],
                 last_name    = data["last_name"],
                 email        = email,
-                password     = password,
+                password     = secret_password,
                 phone_number = phone_number,
                 
             )
